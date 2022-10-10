393f7827a43acbc6911b91ec52d7cbec09c1bda7
IMG-2017\Stud <stud@ase.ro>
2022-10-10 13:13:36 +0300

    commit

--- BugetulMeu/app/src/main/java/dam/g1081/bugetulmeu/MainActivity.java
+++ BugetulMeu/app/src/main/java/dam/g1081/bugetulmeu/MainActivity.java
@@ -1,8 +1,12 @@
 package dam.g1081.bugetulmeu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // se apeleaza automat la init meniului
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // deserializeaza fisierul .xml si creaza meniul
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.action_despre) {
+            // Intent(componenta_invocatoare, clasa_de_tip_activitate)
+            Intent intent = new Intent(this, DespreActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
