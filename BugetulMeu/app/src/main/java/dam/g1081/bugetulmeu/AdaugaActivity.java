929a43239de29997341a5802bd6420420c9a77e6
IMG-2017\Stud <stud@ase.ro>
2022-10-10 13:13:51 +0300

    commit

---
+++ BugetulMeu/app/src/main/java/dam/g1081/bugetulmeu/AdaugaActivity.java
@@ -0,0 +1,14 @@
+package dam.g1081.bugetulmeu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdaugaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adauga);
+    }
+}
