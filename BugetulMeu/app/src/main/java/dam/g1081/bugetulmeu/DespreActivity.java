253d0b5c500fcee3f64911f8d7f5ae844a682201
IMG-2017\Stud <stud@ase.ro>
2022-10-24 13:22:56 +0300

    seminar 4 sau 5

--- BugetulMeu/app/src/main/java/dam/g1081/bugetulmeu/DespreActivity.java
+++ BugetulMeu/app/src/main/java/dam/g1081/bugetulmeu/DespreActivity.java
@@ -3,6 +3,7 @@ package dam.g1081.bugetulmeu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class DespreActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class DespreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_despre);
+        Button butonok=findViewById(R.id.fabOk);
+        butonok.setOnClickListener(view -> {finish();});
     }
 }
--- BugetulMeu/app/src/main/java/dam/g1081/bugetulmeu/MainActivity.java
+++ BugetulMeu/app/src/main/java/dam/g1081/bugetulmeu/MainActivity.java
@@ -20,29 +20,57 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fabAdauga = findViewById(R.id.fab_adauga);
 
         fabAdauga.setOnClickListener(view -> {
-            // de scris secventa de invocare a activitatii Adauga
+            //de scris secventa de invocare a activitatii adauga
+
+
+                Intent intent1 = new Intent(this,AdaugaActivity.class);
+                startActivity(intent1);
+
         });
+        //FloatingActionButton fabInchide=findViewById(R.id.fabOk);
+        //fabInchide.setOnClickListener(view->finish());
     }
 
-    // se apeleaza automat la init meniului
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // deserializeaza fisierul .xml si creaza meniul
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.main_menu,menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.action_despre) {
-            // Intent(componenta_invocatoare, clasa_de_tip_activitate)
-            // TEMA: de invocat si activitea AdaugaActivity
-            Intent intent = new Intent(this, DespreActivity.class);
-            startActivity(intent);
+       if(item.getItemId()==R.id.action_despre){
+           Intent intent = new Intent(this,DespreActivity.class);
+           startActivity(intent);
+
+           return true;
+       }
+        //tema de invocat si activitatea adauga
+        if(item.getItemId()==R.id.action_adauga){
+            Intent intent1 = new Intent(this,AdaugaActivity.class);
+            startActivity(intent1);
 
             return true;
         }
+        if(item.getItemId()==R.id.action_tranzactie){
+            Intent intent2 = new Intent(this, TranzactieActivity.class);
+            startActivity(intent2);
 
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+
     }
 }
+//clasa tranzactie
+//edit text inputtype numbeer decimal double suma
+//date picker date/local date data date
+//date picker dialog
+//
+//descriere edit text string
+//tip spinner enum venit cheltuieli
+//valuta radiogroup euro/ron enum
+
+//de proiectat interfata
